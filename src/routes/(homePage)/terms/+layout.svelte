1994dae8813b35e7c7b5acee52b287ce36e92221
ignatius22 <igee54077@gmail.com>
2025-12-27 15:54:57 +0100

    Make link redirect to help-center

--- src/routes/(homePage)/terms/+layout.svelte
+++ src/routes/(homePage)/terms/+layout.svelte
@@ -11,12 +11,13 @@
 		<!-- Contact Section -->
 		<div class="flex flex-col items-center gap-4 py-8 text-center">
 			<p class="text-2xl font-bold">Didn’t find what you are looking for?</p>
-			<button
+			<a
 				class="flex cursor-pointer items-center gap-1 rounded-md bg-black px-6 py-2 text-white transition hover:bg-gray-800"
+				href="/help-center"
 			>
 				<img src="/direct-normal.png" alt="icon" class="h-5 w-5" />
 				Contact Us
-			</button>
+		</a>
 		</div>
 	</main>
 </section>

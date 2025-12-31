262846c5defaaecdcadd200d52f5e1d8b338a5b2
Fortstar09 <ogunyafort@gmail.com>
2025-12-31 22:23:33 +0100

    feat: setup event page and include the event tabs

---
+++ src/routes/(app)/(main)/event-page/components/SubPageButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let pageName: string;
+	export let pageText: string;
+	export let link: string;
+</script>
+
+<a href={link} class="flex w-full items-center gap-2 rounded-[12.75px] bg-[#FDFDFD] p-2 ">
+	  <slot />
+<h2 class="merriweather-sans text-sm font-medium inline-flex flex-col  items-start gap-1">
+    {pageName}
+
+{#if pageText}
+    <span class="text-xs font-normal text-[#B8BABA]">{pageText}</span>
+{/if}
+</h2>
+</a>

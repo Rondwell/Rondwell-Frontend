ea1b83cb70d9e2e67f629fa820a36d5860bd11c1
Fortstar09 <ogunyafort@gmail.com>
2026-01-14 00:21:36 +0100

    chore: format the speaker page

--- src/routes/(app)/(main)/event-page/[id]/participant/components/Speakers.svelte
+++ src/routes/(app)/(main)/event-page/[id]/participant/components/Speakers.svelte
@@ -1,9 +1,9 @@
-	<script lang="ts">
+<script lang="ts">
 	import Icon from '@iconify/svelte';
 
 	let searchQuery = '';
 
-export const speakers  = [
+	export const speakers = [
 		{
 			id: 1,
 			name: 'John Odoemenem',
@@ -46,12 +46,12 @@ export const speakers  = [
 		}
 	];
 
-		const handleStatus: any = {
+	const handleStatus: any = {
 		confirm: {
 			label: 'Confirm',
 			color: 'bg-green-100 text-green-700'
 		},
-        approved: {
+		approved: {
 			label: 'Approved',
 			color: 'bg-green-100 text-green-700'
 		},
@@ -63,15 +63,15 @@ export const speakers  = [
 			label: 'Decline',
 			color: 'bg-red-100 text-red-700'
 		},
-		applied:{
+		applied: {
 			label: 'Applied',
 			color: 'bg-orange-100 text-orange-700'
 		},
-		live:{
+		live: {
 			label: 'Live',
 			color: 'bg-cyan-100 text-cyan-700'
 		},
-		pending:{
+		pending: {
 			label: 'Pending',
 			color: 'bg-yellow-100 text-yellow-700'
 		},
@@ -84,97 +84,110 @@ export const speakers  = [
 			color: 'bg-gray-100 text-gray-700'
 		}
 	};
-
-	
 </script>
-    
-    <!-- Speaker Section -->
-		<div class="mt-6">
-				<div class="mb-8 flex flex-wrap items-center justify-between gap-4 md:flex-row">
-			<div>
-				<h2 class="mb-1 text-xl font-semibold">Speakers for Megaexe Party </h2>
-				<p class="text-sm text-[#8C8F93]">
+
+<!-- Speaker Section -->
+<div class="mt-6">
+	<div class="mb-8 flex flex-wrap items-center justify-between gap-4 md:flex-row">
+		<div>
+			<h2 class="mb-1 text-xl font-semibold">
+				Speakers for Megaexe Party
+			</h2>
+			<p class="text-sm text-[#8C8F93]">
 				Manage your event's speakers, their profiles, and session assignments.
-				</p>
-			</div>
-			<div>
-				<button
-					
-					class="flex w-full items-center justify-center gap-2 rounded-md bg-black px-5 py-2 font-medium text-white text-sm transition-colors hover:bg-gray-800 sm:w-fit"
-				>
-					<Icon icon="mdi:plus" class="text-xl" />
-					Add Speaker
-				</button>
-				<!-- <AddParticipant bind:open={showAddModal} participant="Speaker" /> -->
-			</div>
+			</p>
 		</div>
 
-		<!-- Search Bar -->
-		<div class="mb-6 flex flex-col justify-between lg:flex-row lg:items-center">
-			<div class="mb-4 md:mb-0 relative w-full max-w-xl">
-				<input
-					type="text"
-					bind:value={searchQuery}
-					placeholder="Search speakers by name, session, or status..."
-					class="h-9 w-full rounded-lg bg-[#FFFFFF] py-2 pr-4 pl-10 text-gray-500 focus:ring-0 focus:outline-none"
+		<div>
+			<button
+				class="flex w-full items-center justify-center gap-2 rounded-md bg-black px-5 py-2 text-sm font-medium text-white transition-colors hover:bg-gray-800 sm:w-fit"
+			>
+				<Icon icon="mdi:plus" class="text-xl" />
+				Add Speaker
+			</button>
+		</div>
+	</div>
+
+	<!-- Search Bar -->
+	<div class="mb-6 flex flex-col justify-between lg:flex-row lg:items-center">
+		<div class="relative mb-4 w-full max-w-xl md:mb-0">
+			<input
+				type="text"
+				bind:value={searchQuery}
+				placeholder="Search speakers by name, session, or status..."
+				class="h-9 w-full rounded-lg bg-white py-2 pl-10 pr-4 text-gray-500 focus:outline-none focus:ring-0"
+			/>
+			<span class="absolute left-3 top-2.5 text-gray-400">
+				<img
+					src="/search-favorite.png"
+					alt="search icon"
+					class="h-5 w-5"
 				/>
-				<span class="absolute top-2.5 left-3 text-gray-400">
-					<img src="/search-favorite.png" alt="search icon" class="h-5 w-5" />
-				</span>
-			</div>
-
-			<div class="flex items-center gap-1 md:flex-row">
-				<div class="flex h-[33px] w-[33px] items-center justify-center rounded-lg bg-[#EBECED]">
-					<img src="/download-icon.svg" alt="download icon" />
-				</div>
-				<div class="flex h-[33px] w-[33px] items-center justify-center rounded-lg bg-[#EBECED]">
-					<img src="/export.svg" alt="export icon" />
-				</div>
-				<button
-						class="flex flex-shrink-0 cursor-pointer items-center gap-2 rounded-md bg-[#EBECED] px-3 py-2 text-xs text-[#616265] md:text-sm"
-					>
-						<Icon icon="mdi:plus" class="h-5 w-5" />
-					Status
-					</button>
-
-					<button
-						class="flex flex-shrink-0 cursor-pointer items-center gap-2 rounded-md bg-[#EBECED] px-3 py-2 text-xs text-[#616265] md:text-sm"
-					>
-						<Icon icon="mdi:plus" class="h-5 w-5" />
-					Has Session
-					</button>
-			</div>
+			</span>
 		</div>
 
-		<div class="overflow-hidden">
-			{#if speakers.length > 0}
+		<div class="flex items-center gap-1">
+			<div class="flex h-[33px] w-[33px] items-center justify-center rounded-lg bg-[#EBECED]">
+				<img src="/download-icon.svg" alt="download icon" />
+			</div>
+
+			<div class="flex h-[33px] w-[33px] items-center justify-center rounded-lg bg-[#EBECED]">
+				<img src="/export.svg" alt="export icon" />
+			</div>
+
+			<button
+				class="flex flex-shrink-0 cursor-pointer items-center gap-2 rounded-md bg-[#EBECED] px-3 py-2 text-xs text-[#616265] md:text-sm"
+			>
+				<Icon icon="mdi:plus" class="h-5 w-5" />
+				Status
+			</button>
+
+			<button
+				class="flex flex-shrink-0 cursor-pointer items-center gap-2 rounded-md bg-[#EBECED] px-3 py-2 text-xs text-[#616265] md:text-sm"
+			>
+				<Icon icon="mdi:plus" class="h-5 w-5" />
+				Has Session
+			</button>
+		</div>
+	</div>
+
+	<div class="overflow-hidden">
+		{#if speakers.length > 0}
 			<div class="overflow-hidden">
 				{#each speakers as s}
 					<div
-						class="flex flex-col justify-between gap-3 border-[0.75px] mb-[11px] px-4 py-3 lg:flex-row lg:items-center rounded-xl bg-white"
+						class="mb-[11px] flex flex-col gap-3 rounded-xl border-[0.75px] bg-white px-4 py-3 lg:flex-row lg:items-center lg:justify-between"
 					>
-						<!-- Icon + Title -->
+						<!-- Speaker -->
 						<div class="flex items-center gap-3">
-							<img src={s.avatar} alt="${s.name} avater" class="h-7 w-7" />
+							<img
+								src={s.avatar}
+								alt="{s.name} avatar"
+								class="h-7 w-7"
+							/>
 							<div class="font-medium">{s.name}</div>
 						</div>
 
-						<div class="text-gray-500 text-left">{s.profile}</div>
-						<div class="w-40 text-[#131517] font-base">{s.sessionNum} Session </div>
-						
+						<div class="text-left text-gray-500">
+							{s.profile}
+						</div>
 
-						<div class="flex items-center justify-between gap-3">
-							<!-- STATUS BADGE -->
+						<div class="w-40 font-base text-[#131517]">
+							{s.sessionNum} Session
+						</div>
+
+						<div class="flex items-center gap-3">
 							<span
-								class="rounded-full px-3 py-1 text-xs font-medium
-									{handleStatus[s.status].color}"
+								class="rounded-full px-3 py-1 text-xs font-medium {handleStatus[s.status].color}"
 							>
 								{handleStatus[s.status].label}
 							</span>
 
-							<!-- Actions menu -->
 							<button class="ml-4 text-gray-400 hover:text-gray-600">
-								<Icon icon="mdi:dots-horizontal" class="h-5 w-5 text-gray-500" />
+								<Icon
+									icon="mdi:dots-horizontal"
+									class="h-5 w-5 text-gray-500"
+								/>
 							</button>
 						</div>
 					</div>
@@ -184,10 +197,13 @@ export const speakers  = [
 			<!-- Empty state -->
 			<div class="flex h-70 flex-col items-center justify-center">
 				<img src="/planning.svg" alt="" class="h-40" />
-				<p class="mt-2 text-lg font-medium text-[#A2ACB2]">No Session added, yet</p>
-				<p class="mt-1 text-sm text-gray-400">Session will display when they are added</p>
+				<p class="mt-2 text-lg font-medium text-[#A2ACB2]">
+					No Session added, yet
+				</p>
+				<p class="mt-1 text-sm text-gray-400">
+					Session will display when they are added
+				</p>
 			</div>
 		{/if}
-		</div>
-
-		</div>
+	</div>
+</div>
